35a587b55e1cb1178dea1a5a5430989a87bcb318
BoyQuang <boyquang@gmail.com>
2025-08-27 20:28:02 -0600

    Update footer.jsx

--- src/components/layouts/footer.jsx
+++ src/components/layouts/footer.jsx
@@ -14,43 +14,17 @@ function AppFooter() {
             className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
           ></a>
           <span className="mb-3 mb-md-0 text-dark fw ">
-            © {year ? year : "Loading..."} code by{" "}
-            <a
-              className="text-warning"
-              href="http://facebook.com/vudovn.354"
-              target="_blank"
-            >
-              {process.env.BOSS}
-            </a>
-            , copyright
+            © {year ? year : "Loading..."} by BoyQuang
           </span>
         </div>
-        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">          
           <li className="ms-3">
             <a
               className="text-muted fw text-warning"
-              href="https://www.facebook.com/vudovn.354"
-              target="_blank"
-            >
-              <i className="fe fe-facebook"></i>
-            </a>
-          </li>
-          <li className="ms-3">
-            <a
-              className="text-muted fw text-warning"
-              href="https://t.me/vudovn"
+              href="https://boyquang.com"
               target="_blank"
             >
               <i className="fe fe-message-circle"></i>
-            </a>
-          </li>
-          <li className="ms-3">
-            <a
-              className="text-muted fw text-warning"
-              href="https://www.instagram.com/_vudovn"
-              target="_blank"
-            >
-              <i className="fe fe-instagram"></i>
             </a>
           </li>
         </ul>
@@ -60,3 +34,4 @@ function AppFooter() {
 }
 
 export default AppFooter;
+
