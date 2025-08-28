a68ce20710b8edca664a406a9be184d32877ce15
BoyQuang <boyquang@gmail.com>
2025-08-28 01:25:31 -0600

    Update MoviePlayer.jsx

--- src/app/xem-phim/[slug]/[tap]/components/MoviePlayer.jsx
+++ src/app/xem-phim/[slug]/[tap]/components/MoviePlayer.jsx
@@ -27,7 +27,7 @@ const VideoPlayer = ({ episode }) => {
      <iframe
         className="video-iframe rounded"
         title={episode.filename || "Video"}
-        src={https://play.vnn69.com?url=episode.link_m3u8}
+        src={episode.link_embed}
         width="100%"
         height="500px"
         frameBorder="0"
@@ -155,6 +155,7 @@ const MoviePlayer = ({ initialData, params }) => {
 };
 
 export default MoviePlayer;
+
 
 
 
