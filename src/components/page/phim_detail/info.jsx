ba84befb113e25081cf7feebbbb54ad6f8172d41
BoyQuang <boyquang@gmail.com>
2025-10-29 05:56:39 -0600

    Update info.jsx

--- src/components/page/phim_detail/info.jsx
+++ src/components/page/phim_detail/info.jsx
@@ -53,20 +53,7 @@ const Info = ({ data }) => {
           
           { label: "Ngôn ngữ", value: lang },
           { label: "Đạo diễn", value: director.join(", ") },
-          /* { label: "Diễn viên", value: actor.join(", ") || "N/A" }, */
-          {
-  label: "Diễn viên",
-  value: actor.length
-    ? actor.map((name, i) => (
-        <React.Fragment key={i}>
-          <a className="tag-dv">
-            {name}
-          </a>
-          {i < actor.length - 1 && ", "}
-        </React.Fragment>
-      ))
-    : "N/A"
-},
+          { label: "Diễn viên", value: actor.join(", ") || "N/A" }, 
           { label: "Thời lượng", value: time },
           {
             label: "Ngày đăng",
@@ -242,6 +229,7 @@ const Info = ({ data }) => {
 };
 
 export default Info;
+
 
 
 
