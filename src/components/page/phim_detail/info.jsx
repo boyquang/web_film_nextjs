77b55b0ccd2510c4a72521f9920b15eb382d7bbb
BoyQuang <boyquang@gmail.com>
2025-11-01 00:23:29 -0600

    Update info.jsx

--- src/components/page/phim_detail/info.jsx
+++ src/components/page/phim_detail/info.jsx
@@ -23,7 +23,7 @@ const Info = ({ data }) => {
     director = ["Đang cập nhật"],
     actor = ["Đang cập nhật"],
     year = "Đang cập nhật",
-    quoc_gia = "Đang cập nhật",
+    countries = "Đang cập nhật",
   } = movie;
 
   const MovieInfo = () => (
@@ -40,7 +40,7 @@ const Info = ({ data }) => {
           { label: "Tổng số tập phim", value: episode_total },
           { label: "Chất lượng", value: quality },
           { label: "Năm", value: year },
-          { label: "Quốc gia", value: quoc_gia },
+          { label: "Quốc gia", value: countries },
         ].map(({ label, value }) => (
           <li key={label}>
             <p>
@@ -229,6 +229,7 @@ const Info = ({ data }) => {
 };
 
 export default Info;
+
 
 
 
